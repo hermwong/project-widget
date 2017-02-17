e2e202c30c6cb4a43142a2074944666a18770b0e
Herm Wong <herm.wong@gmail.com>
2017-02-16 16:06:02 -0800

    cleaned up the layout of the project list item component

--- project-list.js
+++ project-list.js
@@ -1,5 +1,5 @@
-Vue.component('project-list', {
-    template: '<div><project v-for="project in projects">{{ project.name }}</project></div>',
+var projectListComponent = Vue.extend({
+    template: '#project-list',
     data() {
         return {
             projects: [
@@ -7,35 +7,44 @@ Vue.component('project-list', {
                     name: 'hello world',
                     version: 'v1.0',
                     icon: 'icon.png',
-                    localPath: 'http://www.google.com',
+                    localPath: 'http://www.google.com'
                 },
                 {
                     name: 'star track',
                     version: 'v1.0',
                     icon: 'icon.png',
-                    localPath: 'http://www.google.com',
+                    localPath: 'http://www.google.com'
                 },
                 {
                     name: 'magical todo list',
                     version: 'v1.0',
                     icon: 'icon.png',
-                    localPath: 'http://www.google.com',
+                    localPath: 'http://www.google.com'
                 },
                 {
                     name: 'phonegap',
                     version: 'v1.0',
                     icon: 'icon.png',
-                    localPath: 'http://www.google.com',
+                    localPath: 'http://www.google.com'
                 }
             ]
         };
     }
 });
 
-Vue.component('project', {
-    template: '<li><slot></slot></li>'
+var projectListItemComponent = Vue.extend({
+    template: '#project-list-item',
+    props: ['item'],
+    data() {
+        return {
+            item: {}
+        }
+    }
 });
 
+Vue.component('project-list', projectListComponent);
+Vue.component('project-list-item', projectListItemComponent);
+
 new Vue({
-    el: '#root'
+    el: '#root',
 });
